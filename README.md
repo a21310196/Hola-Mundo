e33ad6bb73d4afc4a43b7e21afdacbc3af61df3b
a21310196 <160084874+a21310196@users.noreply.github.com>
2024-02-15 02:07:28 +0000

    Version 0

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Hola-Mundo
+## practica1
---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+   using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+   cout<<"Hola"<<"21310196"<<endl;
+
+    return 0;
+}
